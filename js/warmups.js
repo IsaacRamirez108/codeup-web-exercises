c182ac59fe896d3fd7147edfe29e2a33c6c43d63
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2022-10-21 10:53:48 -0500

    Completed html exercises

--- js/warmups.js
+++ js/warmups.js
@@ -1,6 +1,5 @@
 (function() {
     "use strict";
-
 let obj = {
     Location: 'San Antonio',
     Price: '$200,000',
@@ -12,7 +11,19 @@ let obj = {
     function getPrice(){
         return "Your total home price is " + obj.Price;
     }
-
  console.log(getPrice());
+
+
+    function thelongestStr(arr){
+        let longestStr = "";
+        for (let str of arr){
+            if(str.length > longestStr.length){
+                longestStr = str;
+            }
+        }
+        return longestStr;
+    }
+    const arr = ['Isaac', 'I', 'Isa', 'Is', 'Isaac Ramirez', 'Isaa'];
+    console.log(thelongestStr(arr));
 
 })();
