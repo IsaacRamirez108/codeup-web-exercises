379ca429d3d7ce2b382a3a7b7b06a6197e79b666
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2022-11-17 15:31:07 -0600

    Completed Exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -9,6 +9,16 @@ $(function() {
     // alert(secondAlert);
 
     // Second JQuery Section Exercises
-    $('.codeup').css('border', '1px solid red');
+    //$('.codeup').css('border', '1px solid red');
 
+    // Third JQuery Section Exercises
+    // $('.codeup').css('font-size', '20px');
+    // $('h1').css('background-color', 'yellow');
+    // $('p').css('background-color', 'yellow');
+    // $('li').css('background-color', 'yellow');
+    // let h1Alert = $('h1').html();
+    // alert(h1Alert);
+
+    // Fourth JQuery Section Exercises
+    $('h1, p, li').css('background-color', 'yellow');
 });
