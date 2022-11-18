24fea6822a278896a67b9b2cf305bcca7a5f28be
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2022-11-18 11:02:01 -0600

    Completed Exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -20,5 +20,24 @@ $(function() {
     // alert(h1Alert);
 
     // Fourth JQuery Section Exercises testing
-    $('h1, p, li').css('background-color', 'yellow');
+    //$('h1, p, li').css('background-color', 'yellow');
+
+    // Mouse Events Exercises
+    $('h1').click(function() {
+        $(this).css('background-color', '#00B2A9');
+    });
+
+    $('p').dblclick(function() {
+        $(this).css('font-size', '18px');
+    });
+
+    $('li').hover(
+        function() {
+            $(this).css('color', 'red');
+        },
+        function() {
+            $(this).css('color', 'black');
+        }
+    );
+
 });
